6233e2e0cf45d46a9bb9b1e5dd35716a85c4b190
eecmanny <eec123manny@gmail.com>
2023-08-28 06:06:02 -0400

    basic-code

--- db/schema.sql
+++ db/schema.sql
@@ -1,27 +1,47 @@
 DROP DATABASE IF EXISTS myCompany_db;
-CREATE DATABASE company_db;
+CREATE DATABASE myCompany_db;
 
-USE company_db;
+USE myCompany_db;
 
-CREATE TABLE Employee (
+CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  movie_name VARCHAR(100) NOT NULL
+  department_name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE Department (
+CREATE TABLE employeeRole (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    movie_id INT,
-    review TEXT NOT NULL,
-    FOREIGN KEY (movie_id)
-    REFERENCES movies(id)
+    department_id INT,
+    title VARCHAR(30) NOT NULL
+    salary DECIMAL
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
     ON DELETE SET NULL
 );
 
-CREATE TABLE EmployeeRole (
+CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    movie_id INT,
-    review TEXT NOT NULL,
-    FOREIGN KEY (movie_id)
-    REFERENCES movies(id)
+    role_id INT,
+    manager_id INT,
+    first_name VARCHAR(40) NOT NULL
+    last_name VARCHAR(50) NOT NULL
+    FOREIGN KEY (employeeRole_id)
+    REFERENCES employeeRole(id)
     ON DELETE SET NULL
 );
+
+
+-- CREATE TABLE customers (
+--   id INT NOT NULL,
+--   first_name VARCHAR(30),
+--   last_name VARCHAR(30),
+--   PRIMARY KEY (id)
+-- );
+
+-- CREATE TABLE customer_order (
+--   id INT,
+--   customer_id INT,
+--   order_details TEXT,
+--   FOREIGN KEY (customer_id)
+--   REFERENCES customers(id)
+--   ON DELETE SET NULL
+-- );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,16 +1,30 @@
-INSERT INTO movies (movie_name)
-VALUES ("Lion King"),
-       ("The Godfather"),
-       ("West Side Story"),
-       ("Parasite"),
-       ("The Wizard of Oz");
+-- INSERT INTO movies (movie_name)
+-- VALUES ("Lion King"),
+--        ("The Godfather"),
+--        ("West Side Story"),
+--        ("Parasite"),
+--        ("The Wizard of Oz");
 
-INSERT INTO reviews (movie_id, review)
-VALUES (1, "Zazu is underrated. Give that hornbill a sequel!"),
-       (2, "I'm gonna make him an offer you can't refuse, watch this movie"),
-       (1, "Scar is the lion everyone loves to hate"),
-       (3, "Ten years of ballet and three years of tap to join a gang in this neighborhood"),
-       (5, "The tin man gave a metallic, hollow performance"),
-       (1, "Hakuna matata"),
-       (5, "Those flying monkeys are nightmare fuel!");
+-- INSERT INTO reviews (movie_id, review)
+-- VALUES (1, "Zazu is underrated. Give that hornbill a sequel!"),
+--        (2, "I'm gonna make him an offer you can't refuse, watch this movie"),
+--        (1, "Scar is the lion everyone loves to hate"),
+--        (3, "Ten years of ballet and three years of tap to join a gang in this neighborhood"),
+--        (5, "The tin man gave a metallic, hollow performance"),
+--        (1, "Hakuna matata"),
+--        (5, "Those flying monkeys are nightmare fuel!");
+       
+INSERT INTO customers (id, first_name, last_name)
+VALUES (001, "Arthur", "Miller"),
+       (002, "Chinua", "Achebe"),
+       (003, "Margaret", "Atwood"),
+       (004, "Gabriel", "Garcia Marquez"),
+       (005, "Simone", "de Beauvoir");
+       
+INSERT INTO customer_order (id, customer_id, order_details)
+VALUES (001, 001, "coffee, milk"),
+       (002, 002, "eggs, cheese"),
+       (003, 003, "organic cereal"),
+       (004, 004, "oranges, peaches"),
+       (005, 005, "ice cream");
        
