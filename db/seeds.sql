d2f6744461b473f595aec8357ff1173b992993e4
eecmanny <130661353+eecmanny@users.noreply.github.com>
2023-08-28 16:10:18 -0400

    Update seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,22 +1,22 @@
 INSERT INTO department (department_name)
-VALUES ("Sales"),
-       ("RD"),
-       ("Manufacturing"),
-       ("Shipping");
+VALUES (001, "Sales"),
+       (002, "RD"),
+       (003, "Manufacturing"),
+       (004, "Shipping");
    
 
-INSERT INTO employeeRole (title, salary)
-VALUES ("CEO", 250000.00),
-       ("VP", 175000.00),
-       ("General Manager", 125000.00),
-       ("Supervisor", 85000.00),
-       ("Lead Engineer", 85000.00),
-       ("Engineer", 70000.00),
-       ("Lead Technician", 70000.00),
-       ("Technician", 65000.00),
-       ("Lead Sales Associate", 75000.00),
-       ("Sales Associate", 65000.00),
-       ("Shipper", 55000.00);
+INSERT INTO employeeRole (id, title, salary)
+VALUES (001, "CEO", 250000.00),
+       (002, "VP", 175000.00),
+       (003, "General Manager", 125000.00),
+       (004, "Supervisor", 85000.00),
+       (005, "Lead Engineer", 85000.00),
+       (006, "Engineer", 70000.00),
+       (007, "Lead Technician", 70000.00),
+       (008, "Technician", 65000.00),
+       (009, "Lead Sales Associate", 75000.00),
+       (010, "Sales Associate", 65000.00),
+       (011, "Shipper", 55000.00);
 
 -- VALUES (001, 001, "coffee, milk"),
 --        (002, 002, "eggs, cheese"),
@@ -24,26 +24,26 @@ VALUES ("CEO", 250000.00),
 --        (004, 004, "oranges, peaches"),
 --        (005, 005, "ice cream");
 
-INSERT INTO employee (first_name, last_name)
-VALUES ("Arthur", "Miller"),
-       ("Chinua", "Achebe"),
-       ("Margaret", "Atwood"),
-       ("Gabriel", "Garcia Marquez"),
-       ("Simone", "de Beauvoir"),
-       ("Manny", "Cordero"),
-       ("Micheal", "Angelo"),
-       ("Leonardo", "Davinci"),
-       ("Simon", "Cowell"),
-       ("Lenerd", "Nemoi"),
-       ("James", "Kirk"),
-       ("Micheal", "Jackson"),
-       ("Tom", "Hanks"),
-       ("John", "Doe"),
-       ("Jane", "Doe"),
-       ("Chris", "Smith"),
-       ("Jen", "Smith"),
-       ("Rick", "Sanchez"),
-       ("David", "Schitts"),
-       ("John", "Connor"),
-       ("Alexus", "Schitts");
+INSERT INTO employee (role_id, first_name, last_name)
+VALUES (011, "Arthur", "Miller"),
+       (010, "Chinua", "Achebe"),
+       (010, "Margaret", "Atwood"),
+       (007, "Gabriel", "Garcia Marquez"),
+       (008, "Simone", "de Beauvoir"),
+       (001, "Manny", "Cordero"),
+       (010, "Micheal", "Angelo"),
+       (003, "Leonardo", "Davinci"),
+       (010, "Simon", "Cowell"),
+       (003, "Lenerd", "Nemoi"),
+       (008, "James", "Kirk"),
+       (008, "Micheal", "Jackson"),
+       (006, "Tom", "Hanks"),
+       (007, "John", "Doe"),
+       (011, "Jane", "Doe"),
+       (004, "Chris", "Smith"),
+       (004, "Jen", "Smith"),
+       (005, "Rick", "Sanchez"),
+       (009, "David", "Schitts"),
+       (011, "John", "Connor"),
+       (002, "Alexus", "Schitts");
 
