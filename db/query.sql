e55822145872ccd6385dbaf200413e53426ddd42
eecmanny <eec123manny@gmail.com>
2023-08-29 22:52:33 -0400

    query-join

--- db/query.sql
+++ db/query.sql
@@ -1,8 +1,21 @@
 SELECT *
-FROM myCompany_db
+FROM myCompany_db;
 
-SELECT department, COUNT(id) AS department, department_name,
-FROM myCompany_db
-JOIN
-ON 
-ORDER BY ;
+SELECT title,salary, department_id, department_name
+FROM role
+LEFT JOIN department
+ON role.department_id = department.id;
+
+
+SELECT first_name,last_name, role_id, manager_id, title,salary, department_id, department_name
+FROM employee
+LEFT JOIN role
+ON employee.role_id = role.id
+LEFT JOIN department
+ON role.department_id = department.id;
+
+
+-- JOIN student_course
+--   ON student.id = student_course.student_id
+-- JOIN course
+--   ON course.id = student_course.course_id;
